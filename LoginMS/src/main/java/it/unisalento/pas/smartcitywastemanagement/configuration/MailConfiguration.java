f847d1cd31afe205ef5c7d016d714a7f544193dc
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-08-23 18:20:27 +0200

    Added system configuration for sending emails using Gmail SMTP server

---
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/configuration/MailConfiguration.java
@@ -0,0 +1,54 @@
+package it.unisalento.pas.smartcitywastemanagement.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("wastemanagementapp.info@gmail.com");
+        mailSender.setPassword("njkocgwejsbsczqd");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+
+    // BEAN per la definizione del template email per comunicare le credenziali di accesso al cittadino
+    @Bean
+    public SimpleMailMessage templateCredentialsMessage() {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setText(
+                "Hey you \uD83D\uDC40,\n" +
+                        "Congratulations! You're now part of our Trash Busters community, and your trashy adventure is about to begin with us! ♻\uFE0F\uD83D\uDDD1\uFE0F\n\n" +
+                        "Hold tight, here are your secret access codes:\n" +
+                        "- Trash Hero Name: %s\n" +
+                        "- Initial Trashy Password: %s\n\n" +
+                        "Remember, change this password at your first login!\n\n" +
+                        "Thanks for joining us on this waste-reducing rendezvous!\n"+
+                        "Let's make this planet a cleaner place, one bin at a time! \uD83D\uDEAE\uD83C\uDF0E\n\n\n" +
+                        "Trashy high-fives \uD83D\uDD90\uFE0F,\n" +
+                        "The Cleanup Crew");
+         message.setSubject("Your trashy account credentials");
+
+        return message;
+    }
+}
