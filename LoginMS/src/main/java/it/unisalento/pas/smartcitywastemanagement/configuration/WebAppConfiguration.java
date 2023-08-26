b86c84521afd7e1a75424b6fc8fac288e277eebd
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2023-08-26 17:42:13 +0200

    Added CORS policy configuration

---
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/configuration/WebAppConfiguration.java
@@ -0,0 +1,12 @@
+package it.unisalento.pas.smartcitywastemanagement.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebAppConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST");
+    }
+}
