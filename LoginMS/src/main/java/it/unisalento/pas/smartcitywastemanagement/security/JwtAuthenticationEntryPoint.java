2e12fd5bac8891571860598579ea13b4f46a5f0b
carmine.accogli01 <carmine.accogli01@gmail.com>
2023-11-15 18:54:59 +0100

    Fixing

--- LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/security/JwtAuthenticationEntryPoint.java
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/security/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,11 @@
 package it.unisalento.pas.smartcitywastemanagement.security;
 
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -15,12 +17,12 @@ import java.io.Serializable;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
 
-    private static final long serialVersionUID = -7858869558953243875L;
+    //private static final long serialVersionUID = -7858869558953243875L;
 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setHeader("WWW-Authenticate", "Bearer, error=\"unauthorized\", error_description=\"" + authException.getMessage() + "\"");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            response.setHeader("WWW-Authenticate", "Bearer, error=\"unauthorized\", error_description=\"" + authException.getMessage() + "\"");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
