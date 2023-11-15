d664e662d24fd419ffe901a7bae46334a0c3f2df
carmine.accogli01 <carmine.accogli01@gmail.com>
2023-11-15 18:53:55 +0100

    Added mvc configuration and 403/401 status

--- LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/configuration/SecurityConfig.java
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/configuration/SecurityConfig.java
@@ -1,11 +1,14 @@
 package it.unisalento.pas.smartcitywastemanagement.configuration;
 
+import it.unisalento.pas.smartcitywastemanagement.security.AccessDeniedHandlerJwt;
 import it.unisalento.pas.smartcitywastemanagement.security.JwtAuthenticationEntryPoint;
 import it.unisalento.pas.smartcitywastemanagement.security.JwtAuthenticationFilter;
 import it.unisalento.pas.smartcitywastemanagement.service.CustomUserDetailsService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -14,11 +17,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableMethodSecurity
+@EnableWebMvc
 public class SecurityConfig {
 
     @Autowired
@@ -26,6 +34,9 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private AccessDeniedHandlerJwt deniedHandlerEntryPoint;
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -56,11 +67,16 @@ public class SecurityConfig {
                 requestMatchers("/api/authentication/password_reset_token").permitAll().
                 anyRequest().authenticated().
                 and().
-                exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint)).
+                exceptionHandling().
+                  authenticationEntryPoint(jwtAuthenticationEntryPoint).
+                    accessDeniedHandler(deniedHandlerEntryPoint).
+                and().
+        //exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint)).
                 sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
---
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/security/AccessDeniedHandlerJwt.java
@@ -0,0 +1,23 @@
+package it.unisalento.pas.smartcitywastemanagement.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class AccessDeniedHandlerJwt implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
