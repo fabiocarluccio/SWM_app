622d1d3aa8ad9edb60b8045e60b7c37cfd7a371a
carmine.accogli01 <carmine.accogli01@gmail.com>
2024-01-24 19:09:26 +0100

    .gitignore added element

---
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/security/SecurityConstants.java
@@ -0,0 +1,13 @@
+package it.unisalento.pas.smartcitywastemanagement.security;
+
+import java.util.List;
+
+public class SecurityConstants {
+    public static final String JWT_SECRET = "seedseedseedseedseedseedseedseedseedseedseed"; // sarà utilizzato per l'algoritmo di firma
+
+    public static final String ISSUER ="http://loginService:8080";
+
+    public static final String THIS_MICROSERVICE ="http://loginService:8080";
+
+    public static final List<String> AUDIENCE = List.of("http://smartBinsManagementService:8081","http://citizenManagementService:8082","http://disposalManagementService:8083","http://taxService:8085", "http://loginService:8080","http://alarmsMS:8085");
+}
