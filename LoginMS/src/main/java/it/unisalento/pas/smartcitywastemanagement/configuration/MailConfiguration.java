3fb35bf118f4e74fd7ea2b08e8ec720ac97db811
carmineaccogli <112572172+carmineaccogli@users.noreply.github.com>
2024-02-06 15:16:47 +0100

    changed

--- LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/configuration/MailConfiguration.java
+++ LoginMS/src/main/java/it/unisalento/pas/smartcitywastemanagement/configuration/MailConfiguration.java
@@ -19,7 +19,7 @@ public class MailConfiguration {
         mailSender.setPort(587);
 
         mailSender.setUsername("wastemanagementapp.info@gmail.com");
-        mailSender.setPassword("wqjaeskupnwhdhuq");
+        mailSender.setPassword("asdjasdja");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
